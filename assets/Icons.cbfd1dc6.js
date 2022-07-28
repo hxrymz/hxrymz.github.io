4b3b1c3b7bae7e2f68661ff269e9f781012bb11b
Hector Ricardom <hectoricardom@gmail.com>
2022-07-28 10:04:43 -0400

    juerdfks

---
+++ assets/Icons.cbfd1dc6.js
@@ -0,0 +1,1 @@
+import{r as i,g as d,j as s,b as r}from"./index.99da3eb9.js";function g(e){let a=e.name;const[n]=i.exports.useState(d()),t=r(),c=t&&t[a];let o=e.size?e.size:24,l=e.color?e.color:"#5c5c5c";return s("svg",{className:`_Icons_${n}_`,fill:l,height:o,viewBox:"0 0 24 24",width:o,children:s("path",{d:c||""})})}export{g as default};
