0f195dea17826f3ce409994267fc0bae8db5e0f7
Hector Ricardo <hectoricardom@gmail.com>
2022-12-12 21:02:38 -0500

    kngcfxco87yuvt

--- assets/CheckBox.a9a5c787.js
+++
@@ -1,1 +0,0 @@
-import{a as s}from"./index.e74dfeec.js";const m=({data:e,active:i,updSelect:l})=>s("div",{className:"_dsplFlx main_chckbx ",children:e==null?void 0:e.map(c=>s("div",{className:"item_chckbx ","is-active":`${i===c.id}`,onClick:()=>l(c.id),children:c.label},c.id))});export{m as default};
